8b083d35a7e22503f8f97054fd00a40a4cc4dea4
Leonel <luisleonelmtz@gmail.com>
2025-11-27 15:54:53 -0700

    feat(desktop): Implementa lógica para eliminar, repetir y calcular subtotal en POS

--- FarmaciaSistema.API/Data/FarmaciaSistemaDbContext.cs
+++ FarmaciaSistema.API/Data/FarmaciaSistemaDbContext.cs
@@ -9,7 +9,7 @@ namespace FarmaciaSistema.API.Data
         {
         }
 
-        // Define un DbSet por cada entidad que quieras que se convierta en tabla
+    
         public DbSet<Producto> Productos { get; set; }
         public DbSet<Proveedor> Proveedores { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
@@ -18,7 +18,6 @@ namespace FarmaciaSistema.API.Data
         public DbSet<CompraDetalle> ComprasDetalles { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
 
-        // Dentro de la clase FarmaciaSistemaDbContext
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
